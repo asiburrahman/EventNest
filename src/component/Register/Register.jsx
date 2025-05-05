1925ad61e497b4bdf1430e443e833db0f615b01f
Asibur Rahman <asibur70@gmail.com>
2025-05-05 23:00:52 +0600

    add pssword validation

--- src/component/Register/Register.jsx
+++ src/component/Register/Register.jsx
@@ -5,11 +5,14 @@ import { AuthContext } from '../../context/AuthContext';
 import { toast } from 'react-toastify';
 import { FaEye } from "react-icons/fa";
 import { FaEyeSlash } from "react-icons/fa";
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
 // import { auth } from '../../firebase.init';
 
 const Register = () => {
   const userInfo = use(AuthContext)
   const [showPassword, setShowPassword] = useState(false)
+  const [errorMessage, setErrorMessage] = useState('')
   const {createUser, updateUserProfile, setUser, user} = userInfo
   const navigate = useNavigate()
 
@@ -21,6 +24,18 @@ const Register = () => {
         const email = e.target.mail.value
         const password = e.target.password.value
          console.log(name, photoUrl, email, password);
+
+         setErrorMessage('')
+
+         const passwordCheker = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/
+
+         if (passwordCheker.test(password)=== false) {
+          setErrorMessage("Password must have one lowercase, one uppercase, one digit and 8 characters or longer")
+          return;
+         }
+
+         
+         
         //password Authentication
        
        
@@ -38,20 +53,22 @@ const Register = () => {
 
               setUser({...userinfo, name, photoUrl})
               console.log(userinfo);
-              toast.success("User Login Successful!!");
+              toast.success("User Create Successful!!");
               // setUser({...user, displayName: name, photoURL: photoUrl})
               // console.log(user);
               
               
             }).catch((error) => {
              console.log(error);
+             
              setUser(userinfo)
              
             });
             navigate('/')
             
           }).catch(error=>{
-            console.log(error, );
+            setErrorMessage(error.message );
+            
             
           })
 
@@ -63,27 +80,33 @@ const Register = () => {
    
     return (
         <div className="card bg-base-100 mt-20 w-full mx-auto max-w-sm shrink-0 shadow-2xl">
+          <ToastContainer />
       <div className="card-body">
       <h1 className="text-2xl font-bold text-center">Register now!</h1>
         <form onSubmit={handleRegister} className="fieldset">
           <label className="label">User Name</label>
-          <input type="text" className="input" name='name' placeholder="User Name" />
+          <input required type="text" className="input" name='name' placeholder="User Name" />
           <label className="label" >User Photo URL</label>
-          <input type="text" className="input" name='photoUrl' placeholder="User Photo Url" />
+          <input required type="text" className="input" name='photoUrl' placeholder="User Photo Url" />
           <label className="label" >Email</label>
-          <input type="email" className="input" name='mail' placeholder="Email" />
+          <input required type="email" className="input" name='mail' placeholder="Email" />
           <label className="label">Password</label>
           <div className='relative'>
-            <input type={showPassword?"text":"password"} className="input" name='password' placeholder="Password" />
+            <input required type={showPassword?"text":"password"} className="input" name='password' placeholder="Password" />
             <button onClick={()=> setShowPassword(!showPassword)} className='btn btn-xs absolute top-2 right-6'>{showPassword? <FaEye />: <FaEyeSlash />} </button>
           </div>
           
-          
-          <button className="btn btn-neutral mt-4">Login</button>
+          {/* (?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,} */}
+          <button className="btn btn-neutral mt-4">Register</button>
         </form>
           
         <p>If You Already Have Account  <NavLink className='text-blue-400' to='/login'>Please Login</NavLink> </p>
+        {errorMessage && <p className='text-red-600 font-bold pt-1'>{errorMessage}</p>
+        
+        }
+       
       </div>
+      
     </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { RouterProvider } from 'react-router'
 import router from './router/Router.jsx'
 import AuthProvider from './context/AuthProvider.jsx'
 import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
